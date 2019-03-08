ecc56c357ce9a036e1b83c7a6384c8dd0b340831
Andrew Bradley <cspotcode@gmail.com>
2019-03-08 18:13:24 -0500

    update catch wrapper

--- transformers/blocksWrappedInTryCatch.psm1
+++ transformers/blocksWrappedInTryCatch.psm1
@@ -1,17 +1,21 @@
 import-module -Force $PSScriptRoot/../core.psm1
 
 function blocksWrappedInTryCatch($ast) {
+  # This wrapper is lifted from https://github.com/PoshCode/PowerShellPracticeAndStyle/issues/37#issuecomment-338117653
+  # There are pros and cons to this approach; we should eventually pick something better, since we can afford the complexity.
+  $try = 'try {'
+  $catch = '} catch {$PSCmdlet.ThrowTerminatingError($PSItem)}'
   extractBefore $ast $ast.body.beginBlock.statements[0]
-  'try {'
+  $try
   extractBlockStatements $ast.body.beginBlock
-  '} catch {throw $_}'
+  $catch
   extractBetween $ast $ast.body.beginBlock.statements[-1] $ast.body.processBlock.statements[0]
-  'try {'
+  $try
   extractBlockStatements $ast.body.processBlock
-  '} catch {throw $_}'
+  $catch
   extractBetween $ast $ast.body.processBlock.statements[-1] $ast.body.endBlock.statements[0]
-  'try {'
+  $try
   extractBlockStatements $ast.body.endBlock
-  '} catch {throw $_}'
+  $catch
   extractAfter $ast $ast.body.endBlock.statements[-1]
 }
