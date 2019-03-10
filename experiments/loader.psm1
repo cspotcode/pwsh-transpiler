8c84e52bc0fd99771390d1ee7c338efae37104d7
Andrew Bradley <cspotcode@gmail.com>
2019-03-10 01:56:10 -0500

    Adds import-compiledmodule stub

--- experiments/loader.psm1
+++ experiments/loader.psm1
@@ -1,3 +1,23 @@
-<#
-Functions to import a module.
-#>
+function Import-CompiledModule($Path) {
+    <#
+    .SYNOPSIS
+    Transpile and Import a module
+    #>
+
+    $sourceText = get-content -raw $Path
+    $sb = [scriptblock]::create($sourceText)
+    $ast = $sb.ast
+
+    # TODO Pass through transformer stack
+
+    # TODO all current transformers assume a FunctionDeclarationAst
+    # Allow transformers to have a *type* so we can invoke them with whole script
+    # or only with function declaration.
+
+    # Compile and execute as a new module
+    $finalAst = [Management.Automation.Language.Parser]::ParseInput(ast.extent.text, $Path, [ref]@(), [ref]@())
+    $moduleScriptBlock = $finalAst.getScriptBlock()
+    $Name = Split-Path -LeafBase $Path
+    $module = New-Module -Name $Name -ScriptBlock $moduleScriptBlock
+    $module | Import-Module
+}
